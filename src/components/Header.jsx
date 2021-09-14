e912062227807f874a2ed4346bf33c3d8aca2b7c
diego alvarez <diego.stonerough@gmail.com>
2021-09-14 15:10:00 -0500

    se implemento menu responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,32 @@
 import "../styles/Header.css";
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   const history = useHistory();
   const [value1, setValue1] = useState("");
   const [value2, setValue2] = useState("");
+  const [menuStatus, setMenuStatus] = useState(false);
 
   const handleSetPokemon = () => {
     if (value1) {
       history.push(`/pokemon/${value1}`)
+      setMenuStatus(false)
     }
   };
 
   const handleSetType = () => {
     if (value2) {
       history.push(`/type/${value2}`)
+      setMenuStatus(false)
     }
   };
+
+  const openCloseMenu = () => {
+    setMenuStatus(!menuStatus)
+  }
 
   return (
     <div className="header">
@@ -26,14 +35,48 @@ const Header = () => {
           src="https://i.imgur.com/VtvmFFL.png"
           alt="logo"
           className="logo"
-        ></img>
-      </div>
-      <div className="search-container">
+        ></img> 
+        {menuStatus
+        ? <FontAwesomeIcon icon={faTimes} className="search-icon" onClick={openCloseMenu}/>
+        : <FontAwesomeIcon icon={faSearch} className="search-icon" onClick={openCloseMenu}/>
+        }       
+      </div>           
+      <div className="search-container1">
         <div>
           <input value={value1} onChange={e => {setValue1(e.target.value)}} placeholder="Name/ID(less than 898)"/>
             <button onClick={handleSetPokemon}>GO!</button>
+        </div>        
+        <div>
+        <select value={value2} onChange={e => {setValue2(e.target.value)}}>
+          <option value="">Select a type</option>
+          <option value="all">All</option>
+          <option value="normal">Normal</option>
+          <option value="fighting">Fighting</option>
+          <option value="flying">Flying</option>
+          <option value="poison">Poison</option>
+          <option value="ground">Ground</option>
+          <option value="rock">Rock</option>
+          <option value="bug">Bug</option>
+          <option value="ghost">Ghost</option>
+          <option value="steel">Steel</option>
+          <option value="fire">Fire</option>
+          <option value="water">Water</option>
+          <option value="grass">Grass</option>
+          <option value="electric">Electric</option>
+          <option value="psychic">Psychic</option>
+          <option value="ice">Ice</option>
+          <option value="dragon">Dragon</option>
+          <option value="dark">Dark</option>
+          <option value="fairy">Fairy</option>
+        </select>
+          <button onClick={handleSetType}>GO!</button>
         </div>
-        
+      </div>
+      <div className="search-container2" style={{display: menuStatus ? "block" : "none" }}>
+        <div>
+          <input value={value1} onChange={e => {setValue1(e.target.value)}} placeholder="Name/ID(less than 898)"/>
+          <button onClick={handleSetPokemon}>GO!</button>
+        </div>        
         <div>
         <select value={value2} onChange={e => {setValue2(e.target.value)}}>
           <option value="">Select a type</option>
